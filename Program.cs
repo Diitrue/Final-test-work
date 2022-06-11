345c6435c1560fe80e858ba14b412848ee7fa58b
Diitrue <d1r007mail@gmail.com>
2022-06-11 12:14:45 +0300

    final

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите строку массива: ");
+string [] mass = new string [5];
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+            array[i] = Console.ReadLine();
+    }
+}
+
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
+FillArray(mass);
+for (int j = 0; j < mass.Length; j++)
+{
+    if (mass[j].Length <= 3)
+    {
+        Console.Write(mass[j]+ " ");
+    }
+}
